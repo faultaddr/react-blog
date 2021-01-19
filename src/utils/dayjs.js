acce2019772f4b38914b16affc6e865557b5cf55
farmer <panyunyi@outlook.com>
2021-01-19 14:20:12 +0800

    change the style of Fragment
    N/A
    
    change the style of Fragment
    
    Signed-off-by: farmer <panyunyi@outlook.com>

--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 
 dayjs.extend(relativeTime)
+dayjs.extend(customParseFormat)
 
 export default dayjs
--- src/views/web/fragments/index.jsx
+++ src/views/web/fragments/index.jsx
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import 'antd/dist/antd.css'
-import {Comment, Tooltip, List, Card, Avatar} from 'antd'
+import { Comment, Tooltip, List, Card, Avatar } from 'antd'
 import axios from '@/utils/axios'
 import useAjaxLoading from '@/hooks/useAjaxLoading'
 import dayjs from '@/utils/dayjs'
-import {RandomId} from 'utils'
+import { RandomId } from 'utils'
 
-const {Meta} = Card
+const { Meta } = Card
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max)) + 1000
@@ -30,16 +30,18 @@ function FragmentList(props) {
   }, [])
 
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}>
       <img
         alt='今日诗词'
         src='https://v2.jinrishici.com/one.svg'
         style={{
           height: '30px',
-          margin: 'auto',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
+          margin: 'auto'
         }}></img>
       <List
         // grid={{
@@ -55,16 +57,26 @@ function FragmentList(props) {
         header={`闲言 【${fragment.length}】`}
         dataSource={fragment}
         split={true}
+        style={{width: '80%', marginTop: '5%', alignSelf: 'center'}}
         renderItem={item => (
-          <List.Item key={item.title}>
-            <List.Item.Meta
-              style={{ display: 'flex', flexDirection: 'row', flex: '1' }}
-              avatar={<Avatar src={'http://www.panyunyi.cn/static/media/avatar.7e58ba1b.jpeg'} />}
-              title={'种菜的小朋友'}
-              description={dayjs(item.createdAt, 'YYYYMMDD').fromNow()}
-            />
-            <div style={{display: 'flex', flexDirection: 'row', flex: '3', justifyContent: 'space-between'}}>
-              <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>{item.content}</div>
+          <List.Item key={item.title} style={{width: '100%'}}>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', width: '100%' }}>
+              {/* <List.Item.Meta
+                style={{ display: 'flex', flexDirection: 'row'}}
+                avatar={<Avatar src={'http://www.panyunyi.cn/static/media/avatar.7e58ba1b.jpeg'} />}
+                title={'种菜的小朋友'}
+                description={dayjs(item.createdAt, 'YYYYMMDD', 'zh-cn').fromNow()}
+              /> */}
+              <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <p style={{justifySelf: 'flex-start', marginLeft: '3%', fontFamily: 'Comic Sans MS', fontSize: 15}}>{item.content}</p>
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                  <div style={{display: 'flex', flexDirection: 'row'}}>
+                    <Avatar style={{marginRight: '10px'}} src={'http://www.panyunyi.cn/static/media/avatar.7e58ba1b.jpeg'} />
+                    <p style={{margin: 'auto', fontSize: 13}}>种菜的小朋友</p>
+                  </div>
+                  <div style={{display: 'flex', justifySelf: 'flex-end', alignSelf: 'flex-end', fontSize: 8}}>{dayjs(item.createdAt, 'YYYYMMDDHHmmss', 'zh-cn').format('YYYY-MM-DD HH:mm:ss') }</div>
+                </div>
+              </div>
             </div>
           </List.Item>
           // <List.Item>
