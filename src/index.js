dff572e28de3ff2d1a2cc320a8a69fdfc84197b7
farmer <cuferpan@gmail.com>
2021-07-20 22:22:28 +0800

    change React-dom to react-dom
    bugfix
    
    change React-dom to react-dom
    
    Signed-off-by: farmer <cuferpan@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'React-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider as BusProvider } from '@/hooks/useBus'
 // redux
