5bfb676016ce84e6fce3685cf189d2e1b601edb8
panyunyi97 <cuferpan@gmail.com>
2022-02-02 01:39:53 +0800

    multiline typer in welcome page
    N/A
    
    multiline typer in welcome page
    
    Signed-off-by: panyunyi97 <cuferpan@gmail.com>

--- src/layout/welcome/index.jsx
+++ src/layout/welcome/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './index.scss'
 import { Layout } from 'antd'
+import {Typer} from './typer'
 const WelcomeLayout = props => {
   function redirect() {
     props.history.push('/home')
@@ -9,8 +10,8 @@ const WelcomeLayout = props => {
     <Layout style={{ width: '100%', height: '100%' }} >
       <div class='container' onClick={redirect}>
 
-        <div class='text-container'><div></div></div>
-
+        {/* <div class='text-container'><div></div></div> */}
+        <Typer/>
         <div class='bird-container bird-container--one'>
           <div class='bird bird--one'></div>
         </div>
---
+++ src/layout/welcome/typer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Typist from 'react-typist'
+import './index.scss'
+
+export class Typer extends React.Component {
+  state = {
+    renderMsg: false,
+  }
+
+  onHeaderTyped = () => {
+    this.setState({ renderMsg: true })
+  }
+
+  render() {
+    const docs = '//github.com/panyunyi97/react-blog'
+    return (
+      <div className='typer'>
+        <Typist className='typer-header' avgTypingDelay={100} startDelay={2000} onTypingDone={this.onHeaderTyped}>
+          <a href={docs}>欢迎来到pyy的菜园子</a>
+        </Typist>
+        <div className='typer-content'>
+          {this.state.renderMsg ? (
+            <Typist className='typer-message' cursor={{ hideWhenDone: true }}>
+              * 这里记录着技术的点滴进步
+              <Typist.Delay ms={1250} />
+              <br />
+              * 这里记录着生活的五彩斑斓
+              <Typist.Delay ms={1250} />
+              <br />
+              * 希望你也一样，做
+              <Typist.Delay ms={500} />
+              资本家的主人
+              <Typist.Backspace count={6} delay={1000} />
+              <Typist.Delay ms={750} />
+              自己的主人
+              <Typist.Delay ms={1250} />
+              <br />
+              <span>
+                *{' '}
+                <a href={docs} className='flash'>
+                  欢迎 PR&Star
+                </a>
+              </span>
+              <br />
+              {''}
+            </Typist>
+          ) : null}
+        </div>
+      </div>
+    )
+  }
+}
